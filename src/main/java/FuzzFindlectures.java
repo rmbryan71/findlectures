c5f86f42e13bb60fad19d56410d8cbf078f5a728
Rob Bryan <rob@rmbryan.com>
2017-09-03 07:59:58 -0400

    added a fuzz, not working yet

--- src/main/java/FuzzFindlectures.java
+++ src/main/java/FuzzFindlectures.java
@@ -1,25 +1,26 @@
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.List;
+import java.util.Random;
 
-public class IsFindlecturesUp {
+
+public class FuzzFindlectures {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", true);
         WebDriver driver = new FirefoxDriver(capabilities);
 
-        String expectedUrl = "http://www.findlectures.com/";
-        driver.get(expectedUrl);
-        try{
-            Assert.assertEquals(expectedUrl,driver.getCurrentUrl());
-            System.out.println("Correct URL");
 
-        }
-        catch(Throwable pageNavigationError){
-            System.out.println("Wrong URL");
+        for (int i = 0; i < 4; i++) {
+            driver.get("http://www.findlectures.com");
+            List<WebElement> elements = driver.findElements(By.tagName("a"));
+            elements.get(new Random().nextInt(elements.size())).click();
         }
     }
 }
